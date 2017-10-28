e00a0fb47cd835d1ac59440566ec4d0df20307aa
Obada Jabassini <obada.jabassini@gmail.com>
2017-10-28 04:17:17 +0200

    add backbone for network arch

---
+++ network.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Network(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+
+def start():
+    pass
+
+if __name__ == '__main__':
+    start()
