036140bf25ac2a71ee08e1920f6e30a53b8b359d
Obada Jabassini <obada.jabassini@gmail.com>
2017-10-31 06:54:04 +0200

    Change the unit test adapting to the new change in sentence embedder class

--- embedder/_tests/test_sentence_embedder.py
+++ embedder/_tests/test_sentence_embedder.py
@@ -1,17 +1,21 @@
 import unittest
 import torch
 
-from embedder.sentence_embedder import SentenceEmbedder
+from embedder.sentence_embedder import SentenceEmbedder, EmbeddingType
 
 
 class SentenceEmbedderTest(unittest.TestCase):
 
     def setUp(self):
         super().setUp()
-        self.embedder = SentenceEmbedder("data/GoogleNews-vectors-negative300.bin")
+        self.max_sentence_length = 14
+        self.embedder = SentenceEmbedder("../../data/GoogleNews-vectors-negative300.bin", EmbeddingType.WORD2VEC, self.max_sentence_length)
 
     def test_embed_sentence(self):
         temp = "small test"
-        # need to fill the tensor
-        tens_a, tens_b = self.embedder.embed(temp), torch.FloatTensor()
-        self.assertTrue(torch.all(torch.lt(torch.abs(torch.add(tens_a, -tens_b)), 1e-12)))
+        temp = self.embedder.embed(temp)
+        self.assertEqual(temp.shape, (self.max_sentence_length, 300))
+
+
+if __name__ == '__main__':
+    unittest.main()
