7b68e355caad62ffb10f778cdef521f0caf67d62
Obada <obada.jabassini@gmail.com>
2017-11-29 03:54:54 +0200

    Add some util methods

--- net/network.py
+++ net/network.py
@@ -8,11 +8,21 @@ import torch.nn.functional as F
 from gensim.models import KeyedVectors
 from torch.autograd import Variable
 from torch.nn import MultiLabelSoftMarginLoss, GRU, Linear, Embedding, DataParallel
-from torch.optim import Adam
+from torch.optim import Adadelta
 from torch.utils.data import DataLoader
 
 from dataset.vqa_dataset import VqaDataset
+from embedder.image_embedder import ImageEmbedder
+from embedder.sentence_embedder import SentenceEmbedder
 from net.gated_hyperbolic_tangent import GatedHyperbolicTangent
+
+
+def to_module(state_dict):
+    new_state_dict = dict()
+    for key in state_dict.keys():
+        new_name = key[key.index(".") + 1:]
+        new_state_dict[new_name] = state_dict[key]
+    return new_state_dict
 
 
 def print_size(name, tensor):
@@ -28,6 +38,34 @@ def create_batch_dir(batch_size: int, base_dir: str = "../models"):
 
 def save_checkpoint(state, epoch: int, directory: str = '../models'):
     torch.save(state, "{}/epoch-{}-checkpoint.pth.tar".format(directory, epoch + 1))
+
+
+def load(model_path: str, info, use_cuda=True):
+    state = torch.load(model_path)
+    state = state["state_dict"]
+    state = to_module(state)
+    net = Network(info["question_vocab_size"] + 1, info["answer_vocab_size"])
+    net.load_state_dict(state)
+    net = DataParallel(net)
+    if use_cuda:
+        net = net.cuda()
+    net.eval()
+    return net
+
+
+def predict(net, info, question, image_path):
+    image_embedder = ImageEmbedder("")
+    sentence_embedder = SentenceEmbedder(info["question_vocab"])
+    image = image_embedder.embed(image_path)
+    image = image.unsequeeze(0)
+    question = sentence_embedder.embed(question)
+    question = question.unsequeeze(0)
+    result = net(question, image)
+    result = result.sequeeze(0).view(info["answer_vocab_size"])
+    _, predicted = result.data.topk(10)
+    predicted = predicted.view(10)
+    answers = info["answer_vocab"]
+    return [answers[pred] for pred in predicted]
 
 
 class Network(nn.Module):
@@ -61,7 +99,8 @@ class Network(nn.Module):
         hidden = hidden[-1]  # size = (batch size, embedding size = 512)
         # print_size("hidden", hidden)
         # image embedding
-        image = F.normalize(image, dim=-1)
+        # dim = -1 ?
+        image = F.normalize(image)
         # concat
         hidden_mat = hidden.repeat(1,
                                    self.image_location_number)  # size = (batch size, image location number * embedding size)
@@ -113,7 +152,7 @@ if __name__ == '__main__':
     net = Network(dataset.questions_vocab_size + 1, dataset.answers_vocab_size, initial_weights)
     net = DataParallel(net).cuda()
     criterion = MultiLabelSoftMarginLoss().cuda()
-    optimizer = Adam(net.parameters(), lr=0.001)
+    optimizer = Adadelta(net.parameters())
     epochs = 100
     print("begin training")
     for epoch in range(epochs):
