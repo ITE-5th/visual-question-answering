88506231368fd8c7651cdc8cd5f11cf18ce084d9
Obada <obada.jabassini@gmail.com>
2017-11-29 03:55:47 +0200

    Soon to be implemented

--- embedder/image_embedder.py
+++ embedder/image_embedder.py
@@ -1,9 +1,44 @@
+import cv2
+import numpy as np
+import torch
+
 from embedder.embedder import Embedder
+
+MIN_BOXES, MAX_BOXES = 36, 36
+
+
+def get_detections_from_im(net, im_file, conf_thresh=0.2):
+    im = cv2.imread(im_file)
+    scores, boxes, attr_scores, rel_scores = im_detect(net, im)
+
+    # Keep the original boxes, don't worry about the regresssion bbox outputs
+    rois = net.blobs['rois'].data.copy()
+    # unscale back to raw image space
+    blobs, im_scales = _get_blobs(im, None)
+
+    cls_boxes = rois[:, 1:5] / im_scales[0]
+    cls_prob = net.blobs['cls_prob'].data
+    pool5 = net.blobs['pool5_flat'].data
+
+    # Keep only the best detections
+    max_conf = np.zeros((rois.shape[0]))
+    for cls_ind in range(1, cls_prob.shape[1]):
+        cls_scores = scores[:, cls_ind]
+        dets = np.hstack((cls_boxes, cls_scores[:, np.newaxis])).astype(np.float32)
+        keep = np.array(nms(dets, cfg.TEST.NMS))
+        max_conf[keep] = np.where(cls_scores[keep] > max_conf[keep], cls_scores[keep], max_conf[keep])
+
+    keep_boxes = np.where(max_conf >= conf_thresh)[0]
+    if len(keep_boxes) < MIN_BOXES:
+        keep_boxes = np.argsort(max_conf)[::-1][:MIN_BOXES]
+    elif len(keep_boxes) > MAX_BOXES:
+        keep_boxes = np.argsort(max_conf)[::-1][:MAX_BOXES]
+    return pool5[keep_boxes]
 
 
 class ImageEmbedder(Embedder):
     def __init__(self, model_path: str):
-        self.embedder = model_path
+        pass
 
-    def embed(self, x):
-        return self.embedder.forward(x)
+    def embed(self, x) -> torch.FloatTensor:
+        return None
